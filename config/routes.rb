19a7e139f58d68133ad0649156104e24a1d5eb20
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-10-20 18:59:04 -0300

    automatted commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,6 @@ Rails.application.routes.draw do
   resources :posts
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
-  authenticate :user, lambda { |u| u.admin? } do
-  mount Sidekiq::Web => '/sidekiq'
-
-  namespace :madmin do
-    resources :impersonates do
-      post :impersonate, on: :member
-      post :stop_impersonating, on: :collection
-    end
-  end
-end
 
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
